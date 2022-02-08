7bb05f28207262c820222cf388f90b93b606e85b
paagr <mason.paolo@gmail.com>
2022-02-08 15:55:23 +0100

    first release

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-drop !bomb on chat.
+# Twitch Drop Game
 
-1:32:00
+Simple Twitch drop game on stream overlay. Type `!ufo <image emote>` in chat to play our game.
+
+>Original game by [Coding Garden](https://coding.garden/). Watch the original video [here](https://www.youtube.com/watch?v=VJFFJAR6AwM).
--- client/app.js
+++ client/app.js
@@ -1,19 +1,24 @@
-const target = document.querySelector('.target');
-console.log(target);
-
-// particles
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
-const drops = [];
+const target = document.querySelector('.target');
+const leaderBoard = document.querySelector('.leader-board');
 
+let drops = [];
+const currentUsers = {};
+let highScores = [];
+
+// create drop element
 function createDropElement(url, isAvatar = false){
+  // create a div
   const div = document.createElement('div');
+  // add a class
   div.className = 'drop';
+  // insert html and pass the attributes
   div.innerHTML = `
-  <img class="bomb" src="images/bomb.png" alt="Bomb">
+  <img class="ufo" src="images/ufo.png" alt="ufo">
   <div class="user-image">
     <img class="${isAvatar ? 'avatar' : ''}" src="${url}">
   </div>`;
@@ -22,88 +27,93 @@ function createDropElement(url, isAvatar = false){
 
 
 
-function doDrop(url, isAvatar = false) {
+function doDrop( { username, url, isAvatar = false }) {
+  currentUsers[username] = true;
+  
+  // create the element
   const element = createDropElement(url, isAvatar);
+
   const drop = {
-    id: Date.now() + Math.random(),
+    username,
     element,
     location: {
       x: window.innerWidth * Math.random(),
       y: -200,
     },
     velocity: {
-      x: Math.random() * (Math.random() > 0.5 ? -1 : 1) * 3,  
+      x: 1 + Math.random() * (Math.random() > 0.5 ? -1 : 1) / 2,
       y: 2 + Math.random() * 3
     }
   };
 
+  // put into array
   drops.push(drop);
   document.body.appendChild(element);
   updateDropPosition(drop);
 }
 
 function updateDropPosition(drop) {
-  
   if(drop.landed) return;
 
   drop.element.style.top = drop.location.y + 'px';
   drop.element.style.left = (drop.location.x - (drop.element.clientWidth / 2)) + 'px';
 }
 
-function update(){
+function update() {
   const targetHalfWidth = target.clientWidth / 2;
   drops.forEach(drop => {
-
     if(drop.landed) return;
 
     drop.location.x += drop.velocity.x;
     drop.location.y += drop.velocity.y;
+    const halfWidth = drop.element.clientWidth / 2;
 
-    const halfWidth = ( drop.element.clientWidth / 2);
     if(drop.location.x + halfWidth >= window.innerWidth) {
       drop.velocity.x = -Math.abs(drop.velocity.x);
     } else if (drop.location.x - halfWidth < 0) {
       drop.velocity.x = Math.abs(drop.velocity.x);
     }
 
-    if(drop.location.y + (drop.element.clientHeight - 7) >= window.innerHeight) {
+    if(drop.location.y + drop.element.clientHeight >= window.innerHeight) {
       drop.velocity.y = 0;
       drop.velocity.x = 0;
-      drop.location.y = window.innerHeight - drop.element.clientHeight;
+      drop.location.y = window.innerHeight - drop.location.y;
       drop.landed = true;
       drop.element.classList.add('landed');
-      const {x} = drop.location; 
-      // const score = ((1-Math.abs(window.innerWidth/2-x))/window.innerWidth/2)*100;
-      const score = Math.abs((window.innerWidth / 10 * 3) - x );
+      const { x } = drop.location;
+      const score = Math.abs(window.innerWidth / 2 - x);
 
       if(score <= targetHalfWidth) {
-        console.log('Target hitted by:', drop);
-        const finalScore = (1-(score/targetHalfWidth)) * 100;
-        console.log(finalScore);
+        const finalScore = (1 - (score / targetHalfWidth)) * 100;
+        leaderBoard.style.display = 'block';
+        highScores.push({
+          username: drop.username,
+          score: finalScore.toFixed(2)
+        });
+        highScores.sort((a, b) => b.score - a.score);
+        highScores = highScores.slice(0, 5);
+        renderLeaderBoard();
       }
+
+      drops = drops.filter(d => d != drop);
+      setTimeout(() => {
+        currentUsers[drop.username] = false;
+        document.body.removeChild(drop.element);
+      }, 90000);
     }
   });
 }
 
-function draw(){
-  drops.forEach(updateDropPosition);
+function renderLeaderBoard() {
+  const scores = leaderBoard.querySelector('.scores');
+  scores.innerHTML = highScores.reduceRight((html, {score, username}) => {
+    return html + `<p>${score} ${username}</p>`;
+  }, '');
 }
 
-// https://static-cdn.jtvnw.net/jtv_user_pictures/2535a3b0-3f79-405b-b9f9-ac51563eda7a-profile_image-70x70.png
-
-const emotes = [
-  'https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/2.0',
-  'https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/2.0',
-  'https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/2.0',
-  'https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/2.0',
-  'https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/2.0',
-];
-
-emotes.forEach((el) => {
-  doDrop(el, false);
-});
-
-doDrop('https://static-cdn.jtvnw.net/jtv_user_pictures/2535a3b0-3f79-405b-b9f9-ac51563eda7a-profile_image-70x70.png', true);
+function draw() {
+  drops.forEach(updateDropPosition);
+}
 
 function gameLoop() {
   update();
@@ -112,3 +122,37 @@ function gameLoop() {
 }
 
 gameLoop();
+
+
+// eslint-disable-next-line no-undef
+const client = new tmi.Client({
+  connection: {
+    secure: true,
+    reconnect: true
+  },
+	channels: [ 'snoozlestudio' ]
+});
+
+client.connect();
+
+client.on('message', (channel, { emotes, username }, message) => {
+	
+  if(message.startsWith('!ufo')){
+
+    if(currentUsers[username]) return;
+
+    const args = message.split(' ');
+    args.shift();
+    const url = args.length ? args[0].trim() : '';
+    if(emotes) {
+      const emoteIds =  Object.keys(emotes);
+      const emote = emoteIds[Math.floor(Math.random() * emoteIds.length)];
+      doDrop({
+        url: `https://static-cdn.jtvnw.net/emoticons/v2/${emote}/default/dark/2.0`,
+        username
+      });
+    } else {
+      console.log(username, url);
+    }
+  }
+});
